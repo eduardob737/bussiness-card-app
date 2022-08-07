5dae98c6c653405482da0b4272549adc637d33fc
edubarbosa <eduardobabrosalhp@gmail.com>
2022-08-07 18:44:47 -0300

    feat: includes database Room and ViewModel

--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -4,18 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.activity.viewModels
+import com.example.businesscard.App
 import com.example.businesscard.R
+import com.example.businesscard.data.viewmodel.MainViewModel
+import com.example.businesscard.data.viewmodel.MainViewModelFactory
 import com.example.businesscard.databinding.ActivityMainBinding
+import com.example.businesscard.ui.adapter.BusinessCardAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.rvCards.adapter = adapter
+
+        getAllBusinessCard()
         insertListener()
 
     }
@@ -24,6 +36,12 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener{
             val intent = Intent(this, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCardList ->
+            adapter.submitList(businessCardList)
         }
     }
 }
